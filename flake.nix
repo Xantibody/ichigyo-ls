58ace7685c2dcd9a7e28616c5f437b3c4e267429
Xantibody <zeku.bushinryu38@gmail.com>
2026-02-14 01:31:50 +0900

    test: add integration tests with real textlint execution
    Add textlint to Nix flake for testing. Create fixture markdown
    with known prh errors. Verify full pipeline: textlint JSON parse,
    fix.range offset-to-position conversion, and text replacement.

--- flake.nix
+++ flake.nix
@@ -36,6 +36,22 @@
             "rust-analyzer"
           ];
         };
+        textlintrc = (pkgs.formats.json { }).generate "textlintrc" {
+          plugins = {
+            "@textlint/markdown" = true;
+          };
+          rules = {
+            preset-ja-technical-writing = {
+              ja-no-mixed-period = false;
+              no-exclamation-question-mark = false;
+            };
+            write-good = true;
+            prh.rulePaths = [
+              "${pkgs.textlint-rule-prh}/lib/node_modules/textlint-rule-prh/node_modules/prh/prh-rules/media/techbooster.yml"
+              "${pkgs.textlint-rule-prh}/lib/node_modules/textlint-rule-prh/node_modules/prh/prh-rules/media/WEB+DB_PRESS.yml"
+            ];
+          };
+        };
         treefmtEval = treefmt-nix.lib.evalModule pkgs {
           projectRootFile = "flake.nix";
           programs.nixfmt.enable = true;
@@ -50,7 +66,18 @@
           buildInputs = with pkgs; [
             rustToolchain
             just
+            (textlint.withPackages [
+              textlint-rule-preset-ja-technical-writing
+              textlint-rule-prh
+              textlint-rule-write-good
+              "@textlint/markdown"
+            ])
           ];
+
+          shellHook = ''
+            [ -f .textlintrc ] && unlink .textlintrc
+            ln -s ${textlintrc} .textlintrc
+          '';
         };
       }
     );
